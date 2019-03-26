0c46cf406a8436ab5ad70ae55617d2dc6b1c81a5
Emaikwu Innocent <ennosaint@outlook.com>
2019-03-26 09:02:04 +0100

    added google fonts and tweaked the navigation logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset("css/app.css")}}">
-        <title></title>
         <!-- Fonts -->
-        <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"> -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Comfortaa:300,400|Oxygen|Roboto:100,300,400,500" rel="stylesheet">
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
         <script src="http://malsup.github.com/jquery.cycle2.js"></script>
+        <title></title>
     </head>
     <body>
       <div id="app"></div>
